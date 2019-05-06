052afda458fc75b6ae64303b5481ff831b97dc14
Adam Farnsworth <pbcopy>
2019-05-05 21:48:53 -0700

    Basic layout

---
+++ src/layouts/Homepage.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <header class="header">
+      <div class="header-text">
+        <h1>FarnsCo makes</h1>
+        <h2 class="services">Websites</h2>
+      </div>
+    </header>
+    <nav class="nav">
+      <g-link class="nav__link" to="/">Home</g-link>
+      <g-link class="nav__link" to="/about">About</g-link>
+      <g-link class="nav__link" to="/credits">Credits</g-link>
+    </nav>
+    <div class="layout">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<static-query>
+query {
+  metaData {
+    siteName
+  }
+}
+</static-query>
+
+<style lang="scss">
+body {
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  line-height: 1.5;
+}
+
+.layout {
+  max-width: 760px;
+  margin: 0 auto;
+  padding-left: 20px;
+  padding-right: 20px;
+}
+
+.header {
+  height: 480px;
+  background-image: url("../assets/dorelys-smits-200737-unsplash.jpg");
+  background-size: cover;
+  .header-text {
+    padding: 200px;
+    h1,
+    h2 {
+      color: white;
+    }
+  }
+}
+
+.nav__link {
+  margin-left: 20px;
+}
+</style>
---
+++ src/pages/Credits.vue
@@ -0,0 +1,236 @@
+<template>
+  <Layout>
+    <h1>Site Credits</h1>
+    <h2>Photo Credits</h2>
+    <ul>
+      <li>
+        <a
+          style="background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px"
+          href="https://unsplash.com/@adoris_photography?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Download free do whatever you want high-resolution photos from Dorelys Smits"
+        >
+          <span style="display:inline-block;padding:2px 3px">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              style="height:12px;width:auto;position:relative;vertical-align:middle;top:-2px;fill:white"
+              viewBox="0 0 32 32"
+            >
+              <title>unsplash-logo</title>
+              <path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path>
+            </svg>
+          </span>
+          <span style="display:inline-block;padding:2px 3px">Dorelys Smits</span>
+        </a>
+      </li>
+      <li>
+        <a
+          style="background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px"
+          href="https://unsplash.com/@davidkovalenkoo?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Download free do whatever you want high-resolution photos from David Kovalenko"
+        >
+          <span style="display:inline-block;padding:2px 3px">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              style="height:12px;width:auto;position:relative;vertical-align:middle;top:-2px;fill:white"
+              viewBox="0 0 32 32"
+            >
+              <title>unsplash-logo</title>
+              <path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path>
+            </svg>
+          </span>
+          <span style="display:inline-block;padding:2px 3px">David Kovalenko</span>
+        </a>
+      </li>
+      <li>
+        <a
+          style="background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px"
+          href="https://unsplash.com/@xshaydx?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Download free do whatever you want high-resolution photos from shayd johnson"
+        >
+          <span style="display:inline-block;padding:2px 3px">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              style="height:12px;width:auto;position:relative;vertical-align:middle;top:-2px;fill:white"
+              viewBox="0 0 32 32"
+            >
+              <title>unsplash-logo</title>
+              <path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path>
+            </svg>
+          </span>
+          <span style="display:inline-block;padding:2px 3px">shayd johnson</span>
+        </a>
+      </li>
+      <li>
+        <a
+          style="background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px"
+          href="https://unsplash.com/@peter_mc_greats?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Download free do whatever you want high-resolution photos from Pietro De Grandi"
+        >
+          <span style="display:inline-block;padding:2px 3px">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              style="height:12px;width:auto;position:relative;vertical-align:middle;top:-2px;fill:white"
+              viewBox="0 0 32 32"
+            >
+              <title>unsplash-logo</title>
+              <path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path>
+            </svg>
+          </span>
+          <span style="display:inline-block;padding:2px 3px">Pietro De Grandi</span>
+        </a>
+      </li>
+      <li>
+        <a
+          style="background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px"
+          href="https://unsplash.com/@cristofer?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Download free do whatever you want high-resolution photos from Cristofer Jeschke"
+        >
+          <span style="display:inline-block;padding:2px 3px">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              style="height:12px;width:auto;position:relative;vertical-align:middle;top:-2px;fill:white"
+              viewBox="0 0 32 32"
+            >
+              <title>unsplash-logo</title>
+              <path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path>
+            </svg>
+          </span>
+          <span style="display:inline-block;padding:2px 3px">Cristofer Jeschke</span>
+        </a>
+      </li>
+      <li>
+        <a
+          style="background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px"
+          href="https://unsplash.com/@cristofer?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Download free do whatever you want high-resolution photos from Cristofer Jeschke"
+        >
+          <span style="display:inline-block;padding:2px 3px">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              style="height:12px;width:auto;position:relative;vertical-align:middle;top:-2px;fill:white"
+              viewBox="0 0 32 32"
+            >
+              <title>unsplash-logo</title>
+              <path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path>
+            </svg>
+          </span>
+          <span style="display:inline-block;padding:2px 3px">Cristofer Jeschke</span>
+        </a>
+      </li>
+      <li>
+        <a
+          style="background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px"
+          href="https://unsplash.com/@cristofer?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Download free do whatever you want high-resolution photos from Cristofer Jeschke"
+        >
+          <span style="display:inline-block;padding:2px 3px">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              style="height:12px;width:auto;position:relative;vertical-align:middle;top:-2px;fill:white"
+              viewBox="0 0 32 32"
+            >
+              <title>unsplash-logo</title>
+              <path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path>
+            </svg>
+          </span>
+          <span style="display:inline-block;padding:2px 3px">Cristofer Jeschke</span>
+        </a>
+      </li>
+      <li>
+        <a
+          style="background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px"
+          href="https://unsplash.com/@cmdrshane?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Download free do whatever you want high-resolution photos from CMDR Shane"
+        >
+          <span style="display:inline-block;padding:2px 3px">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              style="height:12px;width:auto;position:relative;vertical-align:middle;top:-2px;fill:white"
+              viewBox="0 0 32 32"
+            >
+              <title>unsplash-logo</title>
+              <path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path>
+            </svg>
+          </span>
+          <span style="display:inline-block;padding:2px 3px">CMDR Shane</span>
+        </a>
+      </li>
+      <li>
+        <a
+          style="background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px"
+          href="https://unsplash.com/@chadpeltola?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Download free do whatever you want high-resolution photos from Chad Peltola"
+        >
+          <span style="display:inline-block;padding:2px 3px">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              style="height:12px;width:auto;position:relative;vertical-align:middle;top:-2px;fill:white"
+              viewBox="0 0 32 32"
+            >
+              <title>unsplash-logo</title>
+              <path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path>
+            </svg>
+          </span>
+          <span style="display:inline-block;padding:2px 3px">Chad Peltola</span>
+        </a>
+      </li>
+      <li>
+        <a
+          style="background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px"
+          href="https://unsplash.com/@peter_mc_greats?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Download free do whatever you want high-resolution photos from Pietro De Grandi"
+        >
+          <span style="display:inline-block;padding:2px 3px">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              style="height:12px;width:auto;position:relative;vertical-align:middle;top:-2px;fill:white"
+              viewBox="0 0 32 32"
+            >
+              <title>unsplash-logo</title>
+              <path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path>
+            </svg>
+          </span>
+          <span style="display:inline-block;padding:2px 3px">Pietro De Grandi</span>
+        </a>
+      </li>
+    </ul>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Credits"
+  }
+};
+</script>
+<style lang="scss" scoped>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  li {
+    margin: 0 0 5px 0;
+  }
+}
+</style>
